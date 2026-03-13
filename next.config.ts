6cfdb32ad093976a696a3f9297605ae8bc187935
Natanael2412 <natanaelalexandergani@gmail.com>
2026-03-13 15:47:35 +0700

    Merubah prefix asset tetap ke dashboard utama

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  assetPrefix: process.env.NODE_ENV === 'production' ? 'https://app.evory.id' : '',
   images: {
     unoptimized: true,
     remotePatterns: [
